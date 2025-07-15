e72dee6fd9ba28b436f2a58bd3fc7624f446929a
Justine Ace Santos <aces0001@algonquinlive.com>
2025-07-15 12:39:24 -0400

    buttns

--- lib/main.dart
+++ lib/main.dart
@@ -70,27 +70,37 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-         children: [ Text("hello world")]
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+        ),
+        body: Center(
+            child: Column(
+                children: [
+                  const SizedBox(height: 14),
+                  ElevatedButton(
+                    onPressed: (){},
+                    child: const Text('Customers'),
+                  ),
+                  Spacer(),
+                  ElevatedButton(
+                    onPressed: (){},
+                    child: const Text('Airplanes'),
+                  ),
+                  Spacer(),
+                  ElevatedButton(
+                    onPressed: (){},
+                    child: const Text('Flights'),
+                  ),
+                  Spacer(),
+                  ElevatedButton(
+                    onPressed: (){},
+                    child: const Text('Reservation'),
+                  ),
+                ]
+            )
         )
-    )
     );
   }
 
-  void setNewValue(double value)
-  {
-    setState(() {
-      _counter = value;
-    }); //update the GUI to new values
-  }
-
-
-  void buttonClicked(){
-
-  }
 }
